5f3a99b849e84d6570e3abec90a1496c46c453f4
Bob Lokerse <lokerse@hotmail.com>
2020-12-02 15:50:55 +0100

    Day 1 - find two entries that sum up to 2020 and multiply for the answer.

---
+++ Report.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode_Day1_ReportRepair
+{
+    public class Report
+    {
+        private const int Year = 2020;
+        public static int? Fix()
+        {
+            var lines = File.ReadAllLines(@".\input.txt");
+            var list = lines.ToList();
+
+            foreach (var entry in list)
+            {
+                var entryX = int.Parse(entry);
+                var entryY = Year - entryX;
+
+                if (list.Contains(entryY.ToString()))
+                {
+                    Console.WriteLine($"EntryX:{entryX}, EntryY:{entryY}");
+                    return entryX * entryY;
+                }
+            }
+            
+            return null; // 1007104
+        }
+    }
+}
